f71ce7c18e68269da25150166bfb72b93f7ed99b
shahajalal-shawon <shahajalalsheikh001@gmail.com>
2024-05-20 21:54:32 +0600

    created the function and hide the section using by id

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function play(){
+    const homeSection = document.getElementById("home-section");
+    homeSection.classList.add('hidden')
+    // console.log(homeSection.classList);
+    const playgroundSection = document.getElementById("play-ground");
+    playgroundSection.classList.remove('hidden')
+}
